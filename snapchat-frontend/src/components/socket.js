a6ff32e4fea7c2a9cbed7489911dbd35ea3ab4f5
Vishnusaini63 <vs6086764@gmail.com>
2026-04-17 15:50:05 +0530

    vish

--- snapchat-frontend/src/components/socket.js
+++ snapchat-frontend/src/components/socket.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-const socket = io("http://snapchat-vgrt.onrender.com", {
+const socket = io("https://snapchat-vgrt.onrender.com", {
   transports: ["websocket"],
   upgrade: false
 });
