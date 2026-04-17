f0df899d9b3898b00a0c3a2b8d8912acd8d7eb86
Vishnusaini63 <vs6086764@gmail.com>
2026-04-17 12:02:23 +0530

    snap

--- server/config/db.js
+++ server/config/db.js
@@ -1,15 +1,18 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "vishnu@2103", // agar mysql password hai to yaha likho
-  database: "snapchat_clone"
-});
+const db = mysql.createConnection(
+  process.env.DATABASE_URL || {
+    host: "localhost",
+    user: "root",
+    password: "vishnu@2103",
+    database: "snapchat_clone"
+  }
+);
 
 db.connect((err) => {
   if (err) {
-    console.log("Database connection failed ❌");
+    console.log("Database connection failed ❌", err);
   } else {
     console.log("MySQL Connected ✅");
   }
